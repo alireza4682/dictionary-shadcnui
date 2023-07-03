8e54eec722b1ce69460e605b304ea7c787ff1e5c
alireza <comolestyrusistantaneos@gmail.com>
2023-07-03 20:12:36 +0330

    premetives for making cards

--- app/components/oneCard.component.tsx
+++ app/components/oneCard.component.tsx
@@ -1,5 +1,20 @@
 import { oneCardType } from "../store/slices/word.slice";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card.ui";
+import { Separator } from "./ui/separator.ui";
 
-const onCard = (card:oneCardType)=>{
-  
-}
+const OneCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>wow</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>yo</div>
+        <Separator />
+        <div>hey</div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default OneCard;
