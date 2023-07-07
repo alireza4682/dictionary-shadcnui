f0d043bdfeb48159d6142064862bdf1bd7826164
Alireza <comolestyrusistantaneos@gmail.com>
2023-07-07 19:41:21 +0330

    all state has to chang2e

--- app/components/oneCard.component.tsx
+++ app/components/oneCard.component.tsx
@@ -1,12 +1,17 @@
+'use client'
+import  { TData } from "../hooks/queryWord";
 import { oneCardType } from "../store/slices/word.slice";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card.ui";
 import { Separator } from "./ui/separator.ui";
 
-const OneCard = () => {
+const OneCard = (data:TData[]) => {
+
+
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>wow</CardTitle>
+        <CardTitle>{}</CardTitle>
       </CardHeader>
       <CardContent>
         <div>yo</div>
