15a7800faa147d2b1f4336b270310182ebb7766f
alireza <comolestyrusistantaneos@gmail.com>
2023-08-05 15:29:11 +0330

    cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/");
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('#\\:Rlmcq\\:-form-item').clear();
+    cy.get('#\\:Rlmcq\\:-form-item').type('fast');
+    cy.get('.inline-flex').click();
+    cy.get('.pt-0 > :nth-child(1) > :nth-child(1) > .border').click();
+    cy.get(':nth-child(1) > .pt-0 > :nth-child(1) > :nth-child(3) > .border > .h-4').click();
+    /* ==== End Cypress Studio ==== */
+  });
+});
