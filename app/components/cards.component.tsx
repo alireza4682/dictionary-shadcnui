27efca4015b1430e5ee1717ee3ddad6f0bf97fd4
alireza <comolestyrusistantaneos@gmail.com>
2023-08-06 12:05:03 +0330

    ref not compatible

--- app/components/cards.component.tsx
+++ app/components/cards.component.tsx
@@ -1,18 +1,56 @@
 "use client";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 import OneCard from "./oneCard.component";
 import { oneCardType } from "../store/slices/word.slice";
+import { Button } from "@/components/ui/button";
 
 export default function Cards() {
   const allCards = useSelector((store: RootState) => store.main.cards);
+  const itemsRef = useRef<Map<number, Element> | null>(null);
+
+  function scrollToIdx(idx: number) {
+    const map = getMap();
+    const node = map.get(idx);
+    if (node) {
+      node.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center",
+      });
+    }
+  }
+
+  function getMap() {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map<number, Element>();
+    }
+    return itemsRef.current;
+  }
 
   const onCardListChange = useCallback(
     (allCards: oneCardType[]) => {
-      return Array.isArray(allCards)
-        ? allCards.map((c, idx) => <OneCard card={c} key={idx} />)
-        : null;
+      return (
+        <>
+          <Button onClick={() => scrollToIdx(0)}></Button>
+          {Array.isArray(allCards) ??
+            allCards.map((c, idx) => (
+              <OneCard
+                card={c}
+                key={idx}
+                ref={(node: Element) => {
+                  const map = getMap();
+                  if (node) {
+                    map.set(idx, node);
+                  } else {
+                    map.delete(idx);
+                  }
+                }}
+              />
+            ))}
+        </>
+      );
     },
     [allCards]
   );
--- app/components/oneCard.component.tsx
+++ app/components/oneCard.component.tsx
@@ -3,22 +3,28 @@ import { oneCardType } from "../store/slices/word.slice";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { OneWord } from "./oneWord.component";
+import { forwardRef, useRef } from "react";
 
-const OneCard = ({ card }: { card: oneCardType }) => {
-  return (
-    <Card className="min-w-fit max-w-[300px] ">
-      <CardHeader>
-        <CardTitle>{card.headWord}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div>
-          {card.rel.map((c, idx) => (
-            <OneWord wordToShow={c.word} key={idx} />
-          ))}
-        </div>
-      </CardContent>
-    </Card>
-  );
-};
+const OneCard = forwardRef(
+  (
+    { card }: { card: oneCardType },
+    ref: React.HTMLAttributes<HTMLDivElement>
+  ) => {
+    return (
+      <Card className="min-w-fit max-w-[300px] " ref={ref}>
+        <CardHeader>
+          <CardTitle>{card.headWord}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>
+            {card.rel.map((c, idx) => (
+              <OneWord wordToShow={c.word} key={idx} />
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+);
 
 export default OneCard;
