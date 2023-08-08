7b4863028e9869b0db2b90c374e5ba520c4c862b
alireza <comolestyrusistantaneos@gmail.com>
2023-08-08 18:26:30 +0330

    be global ,making card has to change position

--- app/components/cardsContainer.component.tsx
+++ app/components/cardsContainer.component.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import OneCard from "./oneCard.component";
 import { oneCardType } from "../store/slices/word.slice";
 import { Button } from "@/components/ui/button";
@@ -9,26 +9,7 @@ import { ChevronRightIcon, ChevronLeftIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export default function CardsContainer() {
-  const [position, setPosition] = useState(0);
-  const [leftEnable, setLeftEnable] = useState(true);
-  const [rightEnable, setRightEnable] = useState(true);
   const allCards = useSelector((store: RootState) => store.main.cards);
-
-  const onClickLeft = () => {
-    if (position > 0) {
-      if (position - 1 === 0) setLeftEnable(false);
-      if (position === allCards.length - 1) setRightEnable(true);
-      setPosition(position - 1);
-    }
-  };
-
-  const onClickRight = () => {
-    if (position < allCards.length - 1) {
-      if (position + 1 === allCards.length - 1) setRightEnable(false);
-      if (position === 0) setLeftEnable(true);
-      setPosition(position + 1);
-    }
-  };
 
   const onCardListChange = useCallback(
     (allCards: oneCardType[]) => {
@@ -36,14 +17,13 @@ export default function CardsContainer() {
         <div className=" w-full flex flex-row gap-4 items-center justify-center h-[300px]">
           <Button
             size="icon"
-            variant={leftEnable ? "default" : "secondary"}
             className={cn(allCards.length ? "" : "hidden", "z-10")}
           >
             <ChevronLeftIcon />
           </Button>
           <div
             className={cn(
-              "w-1/2 flex flex-row justify-center overflow-x-scroll"
+              "w-1/2 flex flex-row justify-center overflow-x-hidden"
             )}
           >
             {Array.isArray(allCards)
@@ -56,7 +36,6 @@ export default function CardsContainer() {
           </div>
           <Button
             size="icon"
-            variant={rightEnable ? "default" : "secondary"}
             className={cn(allCards.length ? "" : "hidden", "z-10")}
           >
             <ChevronRightIcon />
