87c0e073180907fc53232f5f6dc09db579ad9d04
Alireza <comolestyrusistantaneos@gmail.com>
2023-08-09 00:22:09 +0330

    aasadad

--- app/components/cardsContainer.component.tsx
+++ app/components/cardsContainer.component.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import OneCard from "./oneCard.component";
 import { oneCardType } from "../store/slices/word.slice";
 import { Button } from "@/components/ui/button";
@@ -10,14 +10,37 @@ import { cn } from "@/lib/utils";
 
 export default function CardsContainer() {
   const allCards = useSelector((store: RootState) => store.main.cards);
+  const [position, setPosition] = useState(0);
+  const [leftEnable, setLeftEnable] = useState(false);
+  const [rightEnable, setRightEnable] = useState(false);
+
+  const onClickLeft = () => {
+    if (position > 0) {
+      if (position === allCards.length - 1) setRightEnable(true);
+      setPosition(position - 1);
+      if (position === 0) setLeftEnable(false);
+    }
+  };
+  const onClickRight = () => {
+    if (position < allCards.length - 1) {
+      if (position === 0) setLeftEnable(true);
+      setPosition(position + 1);
+      if (position === allCards.length - 1) setRightEnable(false);
+    }
+  };
 
   const onCardListChange = useCallback(
     (allCards: oneCardType[]) => {
+      if (allCards.length > 0 && position !== 0) setLeftEnable(true);
+      if (allCards.length > 0 && position !== allCards.length - 1)
+        setRightEnable(true);
       return (
         <div className=" w-full flex flex-row gap-4 items-center justify-center h-[300px]">
           <Button
             size="icon"
             className={cn(allCards.length ? "" : "hidden", "z-10")}
+            onClick={onClickLeft}
+            disabled={!leftEnable}
           >
             <ChevronLeftIcon />
           </Button>
@@ -37,13 +60,15 @@ export default function CardsContainer() {
           <Button
             size="icon"
             className={cn(allCards.length ? "" : "hidden", "z-10")}
+            onClick={onClickRight}
+            disabled={!rightEnable}
           >
             <ChevronRightIcon />
           </Button>
         </div>
       );
     },
-    [allCards]
+    [allCards, position]
   );
 
   return (
