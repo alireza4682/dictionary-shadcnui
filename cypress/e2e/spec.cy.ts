bd498e5814eb9d2f887393f485325a92f9680193
alireza <comolestyrusistantaneos@gmail.com>
2023-08-09 00:55:14 +0330

    cypress config

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,14 @@
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3000/");
+  it("searching for fast and clicking on a resulted word", () => {
+    cy.visit("/");
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#\\:Rlmcq\\:-form-item').clear();
-    cy.get('#\\:Rlmcq\\:-form-item').type('fast');
-    cy.get('.inline-flex').click();
-    cy.get('.pt-0 > :nth-child(1) > :nth-child(1) > .border').click();
-    cy.get(':nth-child(1) > .pt-0 > :nth-child(1) > :nth-child(3) > .border > .h-4').click();
+    cy.get("#\\:Rlmcq\\:-form-item").clear();
+    cy.get("#\\:Rlmcq\\:-form-item").type("fast");
+    cy.get(".inline-flex").click();
+    cy.get(".pt-0 > :nth-child(1) > :nth-child(1) > .border").click();
+    cy.get(
+      ":nth-child(1) > .pt-0 > :nth-child(1) > :nth-child(3) > .border > .h-4"
+    ).click();
     /* ==== End Cypress Studio ==== */
   });
 });
