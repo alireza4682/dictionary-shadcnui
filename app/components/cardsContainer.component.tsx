e576da68b2e037e008594a3ffc974895faab200a
alireza <comolestyrusistantaneos@gmail.com>
2023-08-10 09:13:55 +0330

    conditional rendering cards

--- app/components/cardsContainer.component.tsx
+++ app/components/cardsContainer.component.tsx
@@ -7,6 +7,7 @@ import { goLeft, goRight, oneCardType } from "../store/slices/word.slice";
 import { Button } from "@/components/ui/button";
 import { ChevronRightIcon, ChevronLeftIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import useWindowSize from "../hooks/windowSize";
 
 export default function CardsContainer() {
   const dispatch = useAppDispatch();
@@ -14,12 +15,66 @@ export default function CardsContainer() {
   const position = useSelector((store: RootState) => store.main.position);
   const left = useSelector((store: RootState) => store.main.left);
   const right = useSelector((store: RootState) => store.main.right);
+  const [direction, setDirection] = useState<1 | -1>(-1);
+
+  const windowSize = useWindowSize();
+  const screenSize = () => {
+    if (windowSize.width < 640) {
+      return 1;
+    } else if (windowSize.width < 1024) {
+      return 2;
+    } else return 3;
+  };
 
   const onClickLeft = () => {
     dispatch(goLeft());
+    setDirection(-1);
   };
   const onClickRight = () => {
     dispatch(goRight());
+    setDirection(1);
+  };
+
+  const chooseCardsToShow = (
+    arrayOfCards: oneCardType[],
+    currentPos: number,
+    screen: 1 | 2 | 3
+  ) => {
+    if (Array.isArray(arrayOfCards)) {
+      for (let i = 0; i < arrayOfCards.length; i++) {
+        if (screen === screenSize()) {
+          return (
+            <div className={cn(i === currentPos ? "" : "hidden")} key={i}>
+              <OneCard card={arrayOfCards[i]} />
+            </div>
+          );
+        } else if (screen === screenSize()) {
+          return (
+            <div
+              className={cn(
+                i === currentPos || i === currentPos + direction ? "" : "hidden"
+              )}
+              key={i}
+            >
+              <OneCard card={arrayOfCards[i]} />
+            </div>
+          );
+        } else if (screen === screenSize()) {
+          return (
+            <div
+              className={cn(
+                i === currentPos || i === currentPos + 1 || i === currentPos - 1
+                  ? ""
+                  : "hidden"
+              )}
+              key={i}
+            >
+              <OneCard card={arrayOfCards[i]} />
+            </div>
+          );
+        }
+      }
+    }
   };
 
   const onCardListChange = useCallback(() => {
@@ -36,13 +91,7 @@ export default function CardsContainer() {
         <div
           className={cn("w-1/2 flex flex-row justify-center overflow-x-hidden")}
         >
-          {Array.isArray(allCards)
-            ? allCards.map((card, idx) => (
-                <div className={cn()} key={idx}>
-                  <OneCard card={card} />
-                </div>
-              ))
-            : null}
+          {chooseCardsToShow(allCards, position, 3)}
         </div>
         <Button
           size="icon"
