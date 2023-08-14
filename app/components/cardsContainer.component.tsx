e974df59126b1b47ae9967152f8615de1049f59e
alireza <comolestyrusistantaneos@gmail.com>
2023-08-14 19:21:53 +0330

    ready for priority queue

--- app/components/cardsContainer.component.tsx
+++ app/components/cardsContainer.component.tsx
@@ -6,133 +6,14 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { useCallback } from "react";
 import { useSelector } from "react-redux";
 import useWindowSize from "../hooks/windowSize";
-import {
-  goLeft,
-  goRight,
-  oneCardType,
-  setWindowSize,
-} from "../store/slices/word.slice";
-import { RootState, useAppDispatch } from "../store/store";
+import { oneCardType } from "../store/slices/word.slice";
+import { RootState } from "../store/store";
 import OneCard from "./oneCard.component";
 
 export default function CardsContainer() {
-  const dispatch = useAppDispatch();
   const allCards = useSelector((store: RootState) => store.main.cards);
-  const goLeftEnable = useSelector(
-    (store: RootState) => store.main.goLeftEnable,
-  );
-  const goRightEnable = useSelector(
-    (store: RootState) => store.main.goRightEnable,
-  );
-  const left = useSelector((store: RootState) => store.main.left);
-  const right = useSelector((store: RootState) => store.main.right);
 
   const windowSize = useWindowSize();
-  const screenSize = () => {
-    if (windowSize.width < 640) {
-      dispatch(setWindowSize(0));
-      return 0;
-    } else if (windowSize.width < 1024) {
-      dispatch(setWindowSize(1));
-      return 1;
-    } else {
-      dispatch(setWindowSize(2));
-      return 2;
-    }
-  };
-  const windowWidth = screenSize();
 
-  const onCardListChange = useCallback(() => {
-    const onClickLeft = () => {
-      dispatch(goLeft());
-    };
-    const onClickRight = () => {
-      dispatch(goRight());
-    };
-
-    const chooseCardsToShow = (
-      arrayOfCards: oneCardType[],
-      screen: 0 | 1 | 2,
-    ) => {
-      if (Array.isArray(arrayOfCards)) {
-        if (screen === 1) {
-          return arrayOfCards.map((card, idx) => {
-            return (
-              <div
-                className={cn(idx === left ? "" : "hidden")}
-                key={card.headWord}
-              >
-                <OneCard card={card} />
-              </div>
-            );
-          });
-        } else if (screen === 2) {
-          return arrayOfCards.map((card, idx) => {
-            return (
-              <div
-                className={cn(idx === left || idx === right ? "" : "hidden")}
-                key={card.headWord}
-              >
-                <OneCard card={card} />
-              </div>
-            );
-          });
-        } else {
-          return arrayOfCards.map((card, idx) => {
-            return (
-              <div
-                className={cn(
-                  idx === left || idx === right || idx === (left + right) / 2
-                    ? ""
-                    : "hidden",
-                )}
-                key={card.headWord}
-              >
-                <OneCard card={card} />
-              </div>
-            );
-          });
-        }
-      }
-    };
-    return (
-      <div className=" w-full flex flex-row gap-4 items-center justify-center h-[300px]">
-        <Button
-          size="icon"
-          className={cn(allCards.length ? "" : "hidden", "z-10")}
-          onClick={onClickLeft}
-          disabled={!goLeftEnable}
-        >
-          <ChevronLeftIcon />
-        </Button>
-        <div
-          className={cn("w-fit flex flex-row justify-center overflow-x-hidden")}
-        >
-          {chooseCardsToShow(allCards, windowWidth)}
-        </div>
-        <Button
-          size="icon"
-          className={cn(allCards.length ? "" : "hidden", "z-10")}
-          onClick={onClickRight}
-          disabled={!goRightEnable}
-        >
-          <ChevronRightIcon />
-        </Button>
-      </div>
-    );
-  }, [
-    allCards,
-    goLeftEnable,
-    windowWidth,
-    goRightEnable,
-    dispatch,
-    left,
-    right,
-  ]);
-
-  return (
-    <div className="flex flex-wrap w-3/4 gap-6 justify-center">
-      {onCardListChange()}
-    </div>
-  );
+  return <div className="flex flex-wrap w-3/4 gap-6 justify-center"></div>;
 }
--- app/store/slices/word.slice.ts
+++ app/store/slices/word.slice.ts
@@ -33,11 +33,6 @@ type MainStateType = {
   currentWord: string;
   cards: oneCardType[];
   mode: TendPoint;
-  left: number;
-  right: number;
-  goLeftEnable: boolean;
-  goRightEnable: boolean;
-  windowWidth: 0 | 1 | 2;
 };
 
 const wordSlice = createSlice({
@@ -61,24 +56,10 @@ const wordSlice = createSlice({
       state.cards = state.cards.filter((card) => {
         return JSON.stringify(card) !== JSON.stringify(action.payload);
       });
-      if (!state.cards[state.right] && state.cards[state.right - 1]) {
-        state.right = state.right - 1;
-        if (state.cards[state.left - 1]) state.left = state.left - 1;
-      }
-      if (!state.cards[state.right + 1]) {
-        state.goRightEnable = false;
-      }
-      if (state.cards[state.left - 1]) {
-        state.goLeftEnable = false;
-      }
     },
     removeAllCards: (state) => {
       state.cards = [];
       state.currentWord = "";
-      state.left = 0;
-      state.right = 0;
-      state.goLeftEnable = false;
-      state.goRightEnable = false;
     },
     makeNewCard: (state, action: { payload: TData[]; type: string }) => {
       const exist = state.cards.find(
@@ -95,51 +76,16 @@ const wordSlice = createSlice({
           }),
           mode: state.mode,
         });
-        state.right = state.cards.length - 1;
-        if (state.right - state.left > state.windowWidth) {
-          state.left = state.right - state.windowWidth;
-        }
-        if (state.left > 0) state.goLeftEnable = true;
-        state.goRightEnable = false;
       }
     },
     setMode: (state, action) => {
       state.mode = action.payload;
     },
-    goRight: (state) => {
-      if (state.right < state.cards.length - 1) {
-        state.right++;
-        state.left++;
-      }
-      if (state.right === state.cards.length - 1) state.goRightEnable = false;
-      if (state.left > 0) state.goLeftEnable = true;
-    },
-    goLeft: (state) => {
-      if (state.left > 0) {
-        state.left--;
-        state.right--;
-      }
-      if (state.left === 0) state.goLeftEnable = false;
-      if (state.right < state.cards.length - 1 && state.right > 0) {
-        state.goRightEnable = true;
-      }
-    },
-    setWindowSize: (state, action) => {
-      state.windowWidth = action.payload;
-    },
   },
 });
 
 const { actions, reducer } = wordSlice;
-export const {
-  setHeadWord,
-  removeCard,
-  removeAllCards,
-  makeNewCard,
-  setMode,
-  goLeft,
-  goRight,
-  setWindowSize,
-} = actions;
+export const { setHeadWord, removeCard, removeAllCards, makeNewCard, setMode } =
+  actions;
 const wordReducer = reducer;
 export default wordReducer;
