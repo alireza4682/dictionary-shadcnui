6dc90c9f1ccafe32f7c236461db8fe97d96cdd8a
alireza <comolestyrusistantaneos@gmail.com>
2023-08-16 14:44:04 +0330

    onto something

--- app/components/cardsContainer.component.tsx
+++ app/components/cardsContainer.component.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import useWindowSize from "../hooks/windowSize";
 import { oneCardType } from "../store/slices/word.slice";
@@ -12,21 +12,36 @@ import OneCard from "./oneCard.component";
 
 export default function CardsContainer() {
   const allCards = useSelector((store: RootState) => store.main.cards);
+  const [pos, setPos] = useState(0);
+  const onClickLeft = () => {
+    pos >= 0 && setPos(pos - 1);
+  };
+
+  const onClickRight = () => {
+    pos <= allCards.length - 1 && setPos(pos + 1);
+  };
 
   const windowSize = useWindowSize();
 
   const cardsToShow = (allCards: oneCardType[]) => {
-    return (
-      Array.isArray(allCards) &&
-      allCards.map((card, _) => (
-        <OneCard card={card} key={`${card.headWord} + ${card.mode}`} />
-      ))
-    );
+    return Array.isArray(allCards)
+      ? allCards.map((card, _) => (
+          <OneCard card={card} key={`${card.headWord} + ${card.mode}`} />
+        ))
+      : null;
   };
 
   return (
-    <div className="flex flex-wrap w-3/4 gap-6 justify-center">
-      {cardsToShow(allCards)}
+    <div className="flex flex-wrap w-3/4 gap-6 justify-center items-center">
+      <Button size="icon" onClick={onClickLeft}>
+        <ChevronLeftIcon />
+      </Button>
+      {cardsToShow(allCards)
+        ?.slice()
+        .splice(pos - 3, 3)}
+      <Button size="icon" onClick={onClickRight}>
+        <ChevronRightIcon />
+      </Button>
     </div>
   );
 }
